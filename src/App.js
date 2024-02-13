6dbe14a2c206662f2afcb0e891278c9594a3e342
YolandaTain <yolandataingut@gmail.com>
2024-02-13 12:47:36 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import '../src/assets/css/App.css';
 import Header from './components/header';
 import Library from './components/library';
-import Book from './components/book';
 import Footer from './components/footer';
 
 function App() {
